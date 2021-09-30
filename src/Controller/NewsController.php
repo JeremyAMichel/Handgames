1a72bd5249df1ba2af0b3ffb9ae1b8421a075d28
June <rem-je42@hotmail.fr>
2021-09-30 11:45:25 +0200

    link between routing and navbar fixed

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ActualitesController extends AbstractController
+class NewsController extends AbstractController
 {
     /**
      * @Route("/news", name="news")
--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ProfilGeneralController extends AbstractController
+class ProfilController extends AbstractController
 {
     /**
      * @Route("/profil-general", name="profilGeneral")
