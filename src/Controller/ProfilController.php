de7bbc01d42075ba55b44d8af88cd930e397308e
June <rem-je42@hotmail.fr>
2021-09-30 10:53:22 +0200

    code clean and routing fixed

---
+++ src/Controller/ProfilController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfilGeneralController extends AbstractController
+{
+    /**
+     * @Route("/profil-general", name="profilGeneral")
+     */
+    public function profilGeneral(): Response
+    {
+        return $this->render('profil/profil-general.html.twig', [
+        ]);
+    }
+
+    /**
+     * @Route("/profil-skin", name="profilSkins")
+     */
+    public function profilSkins(): Response
+    {
+        return $this->render('profil/profil-skins.html.twig', [
+        ]);
+    }
+    
+    /**
+     * @Route("/profil-stats", name="profilStats")
+     */
+    public function profilStats(): Response
+    {
+        return $this->render('profil/profil-stats.html.twig', [
+        ]);
+    }
+}
