68eae58e120cb592f5202de306ad19a857b35619
June <rem-je42@hotmail.fr>
2021-10-01 17:27:06 +0200

    choose a profil pic functionnality

---
+++ assets/script/globalScript.js
@@ -0,0 +1,13 @@
+let imgDisplay = document.querySelector("#pp-display");
+
+const profilPics = document.querySelectorAll(".js-choice");
+
+profilPics.forEach(pic => {
+    pic.addEventListener('click', (e)=>{
+        changeProfilPic(pic.src);
+    })
+})
+
+function changeProfilPic(src){
+    imgDisplay.src=src;
+}
