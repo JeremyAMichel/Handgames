b9b1166865898f3924113a6aefced5b1fb5fca07
June <rem-je42@hotmail.fr>
2021-10-11 11:07:18 +0200

    re-organized JS

--- assets/script/globalScript.js
+++ assets/script/globalScript.js
@@ -1,13 +1,1 @@
-let imgDisplay = document.querySelector("#pp-display");
-
-const profilPics = document.querySelectorAll(".js-choice");
-
-profilPics.forEach(pic => {
-    pic.addEventListener('click', (e)=>{
-        changeProfilPic(pic.src);
-    })
-})
-
-function changeProfilPic(src){
-    imgDisplay.src=src;
-}
+import './change-profil-pic-on-click/changeProfilPicOnClick';
