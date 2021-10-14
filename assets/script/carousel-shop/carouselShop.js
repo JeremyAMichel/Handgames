224ec2ad95337afd9cab48491dfe10423a2803d5
June <rem-je42@hotmail.fr>
2021-10-14 18:07:38 +0200

    caroussel next and previous fixed

--- assets/script/carousel-shop/carouselShop.js
+++ assets/script/carousel-shop/carouselShop.js
@@ -4,22 +4,61 @@ carouselSlides[0].classList.add('active');
 carouselSlides[1].classList.add('active');
 carouselSlides[2].classList.add('active');
 
-
+let first = 0;
+let last = 2;
 
 let nextArrow = document.querySelector('.next-carousel-shop');
+let leftArrow = document.querySelector('.previous-carousel-shop');
 
 
 //event fleche de droite
 nextArrow.addEventListener('click', (e)=>{
-    let carouselActives = document.querySelectorAll('.active');
-    rollCarouselLeft(carouselActives[0]);
+    rollCarouselLeft(carouselSlides[first]);
 })
 
 function rollCarouselLeft(leftSlide){
     leftSlide.classList.remove('active');
-    let orderNext = parseInt(leftSlide.getAttribute('data-order'))+3;
+    let orderNext = last+1;
+    
     if(orderNext>5){
-        orderNext-6;
+        orderNext-=6;   
     }
+    
     carouselSlides[orderNext].classList.add('active');
+
+    first++;
+    if(first>5){
+        first-=6;
+    }
+    
+    last++;
+    if(last>5){
+        last-=6;
+    }
+}
+
+//event fleche de gauche
+leftArrow.addEventListener('click', (e)=>{
+    rollCarouselRight(carouselSlides[last]);
+})
+
+function rollCarouselRight(rightSlide){
+    rightSlide.classList.remove('active');
+    let orderPrev = first-1;
+    
+    if(orderPrev<0){
+        orderPrev+=6;   
+    }
+
+    carouselSlides[orderPrev].classList.add('active');
+
+    first--;
+    if(first<0){
+        first+=6;
+    }
+    
+    last--;
+    if(last<0){
+        last+=6;
+    }
 }
