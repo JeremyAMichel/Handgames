a1903baa3cce894bdc555aff503cd65c7a0e8385
nasscore <jonathan.auberix@gmail.com>
2021-10-19 17:19:34 +0200

    back du profil general - stats - news

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -2,19 +2,40 @@
 
 namespace App\Controller;
 
+use App\Repository\ActualiteRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class NewsController extends AbstractController
 {
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    /**
+     * @var ActualiteRepository
+     */
+    private $ar;
+
+    public function __construct(EntityManagerInterface $em, ActualiteRepository $ar)
+    {
+        $this->em=$em;
+        $this->ar=$ar;
+    }
+
     /**
      * @Route("/news", name="news")
      */
     public function news(): Response
     {
-        return $this->render('news/news.html.twig', [
+        $news = $this->ar->findAll();
 
+        return $this->render('news/news.html.twig', [
+            'news'=>$news,
         ]);
     }
 }
