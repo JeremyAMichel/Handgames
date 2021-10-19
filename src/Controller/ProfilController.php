af277c815f3328dfa881e3e2c522f3796dbb6026
June <rem-je42@hotmail.fr>
2021-10-19 13:05:21 +0200

    starting back profil

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,17 +2,59 @@
 
 namespace App\Controller;
 
+use App\Entity\Avatar;
+use App\Repository\AvatarRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfilController extends AbstractController
 {
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    /**
+     * @var AvatarRepository
+     */
+    private $ar;
+
+    public function __construct(EntityManagerInterface $em, AvatarRepository $ar)
+    {
+        $this->em=$em;
+        $this->ar=$ar;
+    }
+   
     /**
      * @Route("/profil-general", name="profilGeneral")
      */
     public function profilGeneral(): Response
     {
+
+        // TEMP
+        $avatars=$this->ar->findAll();
+        if(count($avatars)!= 8){
+            $firstAvatar= $this->ar->findOneBy(['path' => 'build/image/avatars/default-avatar.png']);
+            if(null==$firstAvatar){
+                $firstAvatar= new Avatar();
+                $firstAvatar->setPath('build/image/avatars/default-avatar.png');
+                $this->em->persist($firstAvatar);
+            }
+
+            $secondAvatar= $this->ar->findOneBy(['path' => 'build/image/avatars/profil-pic-1.jpg']);
+            if(null==$secondAvatar){
+                $secondAvatar= new Avatar();
+                $secondAvatar->setPath('build/image/avatars/profil-pic-1.jpg');
+                $this->em->persist($secondAvatar);
+            }
+
+            $this->em->flush();
+        }
+
+        // dump($this->ar->findOneBy(['path' => 'build/image/avatars/']));
         return $this->render('profil/profil-general.html.twig', [
         ]);
     }
