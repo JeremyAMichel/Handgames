a5201ad80292d64cca2ac167b7804998879ee64c
June <rem-je42@hotmail.fr>
2021-10-26 16:45:54 +0200

    code cleaning, and js commentary

--- assets/script/carousel-shop/carouselShop.js
+++ assets/script/carousel-shop/carouselShop.js
@@ -1,6 +1,7 @@
 
 let carouselSlides = document.querySelectorAll('.carousel-slide');
 
+// affichage des 3 première slides
 if (typeof(carouselSlides[0]) != 'undefined' && carouselSlides[0] != null)
 {
     carouselSlides[0].classList.add('active');
@@ -14,14 +15,19 @@ if (typeof(carouselSlides[2]) != 'undefined' && carouselSlides[2] != null)
     carouselSlides[2].classList.add('active');
 }
 
+//indice de la première slide à l'initialisation
 let first = 0;
+
+//indice de la deuxième slide à l'initialisation
 let last = 2;
 
+//on ajoute les class css permettant de gérer l'ordre d'affichage des slides
 addOrderClasses(first, last);
 
 let nextArrow = document.querySelector('.next-carousel-shop');
 let leftArrow = document.querySelector('.previous-carousel-shop');
 
+//timer pour rendre le carousel un peu plus vivant, de sortes qu'il roll à gauche de manière régulière
 let time;
 setNewTimer();
 
@@ -29,6 +35,7 @@ let timeAnimation;
 let timeAnimation2;
 let timeAnimation3;
 
+//variable permettant de gérer si une action est en cours -> pour éviter le spam d'event
 let isAnimationPc = false;
 
 
@@ -36,36 +43,50 @@ let isAnimationPc = false;
 if (typeof(nextArrow) != 'undefined' && nextArrow != null)
 {
     nextArrow.addEventListener('click', (e)=>{
+        // l'event s'active uniquement si une animation n'est pas déjà en cours
         if (isAnimationPc) {
             return;
         }
         isAnimationPc = true;
 
         rollCarouselLeft(carouselSlides[first]);
+
+        // clear le Timer et le relance
         setNewTimer();
 
+        // l'animation dure 1000ms, on bloque donc toute autre animation pendant ce laps de temps
         setTimeout(function() {
             isAnimationPc = false;
         }, 1000);
     })
 }
 
+// fonction qui va permettre au carousel de rouler vers la gauche
 function rollCarouselLeft(leftSlide){
-
+    //sauvegarde de la slide de droite car "last" est incrémenté à la fin de la fonction et celà pose problème
+    //pour les timeout qui utilise la slide de droite
     rightSlide = carouselSlides[last];
 
+    //indice de la prochaine slide (celle que l'ont doit affiché après la slide la plus à droite à l'affichage)
     let orderNext = last+1;
-    orderNext = biggerThanFive(orderNext);
+    orderNext = biggerThanLength(orderNext);
 
+    // next permet de preparer la slide à droite du carousel (elle est cachée par le overflow:hidden du carousel)
     carouselSlides[orderNext].classList.add('next');
 
+    // smooth-trans permet d'avoir des transitions à l'affichage
     leftSlide.classList.add('smooth-trans');
+
+    // soon décale la slide concerné (ici celle de gauche) vers la gauche, jusqu'à qu'elle soit entièrement 
+    // cachée par le overflow hidden du carousel
     leftSlide.classList.add('soon');
 
+    // on détermine quelle est la slide qui est affiché aumilieu lors de l'initialisation
     let mid = first+1;
-    mid = biggerThanFive(mid);
+    mid = biggerThanLength(mid);
 
     carouselSlides[mid].classList.add('smooth-trans');
+    // soon-alt positionne la slide à l'endroit où se trouvait la slide qui la précédait
     carouselSlides[mid].classList.add('soon-alt');
 
     carouselSlides[last].classList.add('smooth-trans');
@@ -75,6 +96,7 @@ function rollCarouselLeft(leftSlide){
         clearTimeout(timeAnimation);
     }
 
+    // au bout d'une seconde -> fonction qui retire les class css residuelle
     timeAnimation = setTimeout(function() {
         removeActiveAndSoonAndNextClassesXL(leftSlide, carouselSlides[mid], rightSlide, carouselSlides[orderNext]);
     }, 1000);
@@ -83,17 +105,17 @@ function rollCarouselLeft(leftSlide){
         clearTimeout(timeAnimation2);
     }
 
+    // au bout d'une demi seconde -> fonction qui gère l'animation 
     timeAnimation2 = setTimeout(function() {
         changeOrders(leftSlide, carouselSlides[mid], rightSlide, carouselSlides[orderNext]);
     }, 500);
 
+    // actualisation de l'indice de la première et dernière slide
     first++;
-    first=biggerThanFive(first);
+    first=biggerThanLength(first);
 
     last++;
-    last=biggerThanFive(last);
-
-
+    last=biggerThanLength(last);
 }
 
 //event fleche de gauche
@@ -114,21 +136,33 @@ if (typeof(leftArrow) != 'undefined' && leftArrow != null)
     })
 }
 
+
+// fonction qui va permettre au carousel de rouler vers la droite
 function rollCarouselRight(rightSlide){
+    // sauvegarde de la slide la plus à gauche car "first" va être incrémenté alors qu'une fonction dans un timeout
+    // a besoin de cette slide
     leftSlide = carouselSlides[first];
 
+    // indice de la slide précédent la slide la plus à gauche à l'affichage
     let orderPrev = first-1;
     orderPrev = lessThanZero(orderPrev);
 
+    // prev permet de preparer la slide à gauche du carousel (elle est cachée par le overflow:hidden du carousel)   
     carouselSlides[orderPrev].classList.add('prev');
 
     rightSlide.classList.add('smooth-trans');
+
+    // prev-soon décale la slide concernée (ici celle de droite) vers la droite, jusqu'à qu'elle soit 
+    // entièrement cachée par le overflow hidden du carousel
     rightSlide.classList.add('prev-soon');
 
+    // on détermine l'indice de la slide affiché au milieu à l'initialisation
     let mid = first+1;
-    mid = biggerThanFive(mid);
+    mid = biggerThanLength(mid);
 
     carouselSlides[mid].classList.add('smooth-trans');
+
+    // prev-soon-alt permet à la slide concernée de se positionner là où se trouvait la slide qui la suivait
     carouselSlides[mid].classList.add('prev-soon-alt');
 
     leftSlide.classList.add('smooth-trans');
@@ -138,14 +172,16 @@ function rollCarouselRight(rightSlide){
         clearTimeout(timeAnimation);
     }
 
+    // fonction qui retire les classes residuelles
     timeAnimation = setTimeout(function() {
-        removeActiveAndSoonAndNextClassesXLPrev(carouselSlides[orderPrev], leftSlide, carouselSlides[mid], rightSlide);
+        removeActiveAndSoonAndNextClassesXLPrev(carouselSlides[orderPrev], rightSlide);
     }, 1000);
 
     if(timeAnimation2 != 'undefined'){
         clearTimeout(timeAnimation2);
     }
 
+    // fonction qui gère l'animation des différentes slides
     timeAnimation2 = setTimeout(function() {
         changeOrdersPrev(carouselSlides[orderPrev], leftSlide, carouselSlides[mid], rightSlide);
     }, 500);
@@ -165,7 +201,7 @@ function addOrderClasses(first, last){
         carouselSlides[last].classList.add('last');
 
         let mid = first+1;
-        mid=biggerThanFive(mid);
+        mid=biggerThanLength(mid);
 
         carouselSlides[mid].classList.add('mid');
     }
@@ -177,7 +213,7 @@ function removeOrderClasses(first, last){
     carouselSlides[last].classList.remove('last');
 
     let mid = first+1;
-    mid=biggerThanFive(mid);
+    mid=biggerThanLength(mid);
 
     carouselSlides[mid].classList.remove('mid');
 }
@@ -191,16 +227,16 @@ function lessThanZero(variable){
     return variable;
 }
 
-//vérifie si la variable n'a pas dépassé une limite (plus grand que 5) et la corrige si c'est le cas
-function biggerThanFive(variable){
-    if(variable>5){
-        variable-=6;
+//vérifie si la variable n'a pas dépassé une limite (plus grand que length de carouselSlides) et la corrige si c'est le cas
+function biggerThanLength(variable){
+    if(variable>(carouselSlides.length-1)){
+        variable-=carouselSlides.length;
     }
 
     return variable;
 }
 
-
+// clear l'ancien timer et en démarre un nouveau
 function setNewTimer(){
  
     clearInterval(time);
@@ -209,6 +245,7 @@ function setNewTimer(){
 
 }
 
+// le timer active manuellement l'event qui fait rouler le carousel à gauche
 function nextSlide(){
     if (typeof(carouselSlides[first]) != 'undefined' && carouselSlides[first] != null)
     {
@@ -225,6 +262,7 @@ function nextSlide(){
     }
 }
 
+// retire les class css résiduelles à la suite d'une animation
 function removeActiveAndSoonAndNextClassesXL(leftSlide, midSlide, rightSlide, nextSlide){
     leftSlide.classList.remove('soon');
     leftSlide.classList.remove('active');
@@ -245,6 +283,7 @@ function removeActiveAndSoonAndNextClassesXL(leftSlide, midSlide, rightSlide, ne
     rightSlide.classList.remove('still');
 }
 
+// gère l'animation vers la droite
 function changeOrders(leftSlide, midSlide, rightSlide, nextSlide){
     leftSlide.classList.remove('first');
 
@@ -265,15 +304,8 @@ function changeOrders(leftSlide, midSlide, rightSlide, nextSlide){
     
 }
 
-function removeNextClassXL(midSlide, rightSlide, nextSlide){
-    midSlide.classList.remove('soon-alt');
-
-    rightSlide.classList.remove('soon-alt');
-
-    nextSlide.classList.remove('next');  
-}
-
-function removeActiveAndSoonAndNextClassesXLPrev(prevSlide, leftSlide, midSlide, rightSlide){
+// retire les class css residuelle suite à l'animation du carousel vers la droite
+function removeActiveAndSoonAndNextClassesXLPrev(prevSlide, rightSlide){
     rightSlide.classList.remove('prev-soon');
     rightSlide.classList.remove('active');
 
@@ -281,6 +313,7 @@ function removeActiveAndSoonAndNextClassesXLPrev(prevSlide, leftSlide, midSlide,
     prevSlide.classList.remove('prev');
 }
 
+// gère l'animation du carousel qui roule vers la droite
 function changeOrdersPrev(prevSlide, leftSlide, midSlide, rightSlide){
     prevSlide.classList.add('first');
 
--- assets/script/carousel-shop/carouselShopMobile.js
+++ assets/script/carousel-shop/carouselShopMobile.js
@@ -1,15 +1,18 @@
 
 let carouselSlidesMobile = document.querySelectorAll('.carousel-slide-mobile');
 
+// on affiche la première slide 
 if (typeof(carouselSlidesMobile[0]) != 'undefined' && carouselSlidesMobile[0] != null)
 {
     carouselSlidesMobile[0].classList.add('active');
 }
 
+// on garde une trace de l'indice de la slide affiché en temps réel
 let activeSlideMobile = 0;
 let nextArrowMobile = document.querySelector('.next-carousel-shop-mobile');
 let leftArrowMobile = document.querySelector('.previous-carousel-shop-mobile');
 
+// timer pour rendre le carousel un peu plus vivant -> trigger une animation de manière régulière
 let timeMobile;
 setNewTimerMobile();
 
@@ -17,11 +20,13 @@ let timeAnimationMobile;
 let timeAnimationMobile2;
 let timeAnimationMobile3;
 
+// variable permettant de savoir si une animation est en cours
 let isAnimationMobile = false;
 
 //event fleche de droite
 if (typeof(nextArrowMobile) != 'undefined' && nextArrowMobile != null)
 {
+    //on lance un event uniquement si une animation n'est pas déjà en cours
     nextArrowMobile.addEventListener('click', (e)=>{
         if (isAnimationMobile) {
             return;
@@ -29,47 +34,57 @@ if (typeof(nextArrowMobile) != 'undefined' && nextArrowMobile != null)
         isAnimationMobile = true;
 
         rollCarouselLeftMobile(carouselSlidesMobile[activeSlideMobile]);
+
+        // fonction qui clear l'ancien timer et en set un nouveau (pour être sur que le timer se déclenche au bout
+        // de xxx secondes d'INACTIVITE)
         setNewTimerMobile();
- 
+        
+        // l'animation dure 1s au total, donc on bloque toute autre animation pendant 1s
         setTimeout(function() {
             isAnimationMobile = false;
         }, 1000);  
     })
 }
 
+//fonction qui s'occupe de faire rouler le carousel sur lui-même en direction de la gauche
 function rollCarouselLeftMobile(activeSlide){
-    let orderNextMobile = activeSlideMobile+1;
-    orderNextMobile = biggerThanFiveMobile(orderNextMobile);
 
+    // on récupère l'indice de la prochaine slide à afficher 
+    let orderNextMobile = activeSlideMobile+1;
+    orderNextMobile = biggerThanLengthMobile(orderNextMobile);
+
+    // la class next permet de préparer la prochaine slide à l'affichage
     carouselSlidesMobile[orderNextMobile].classList.add('next');
     
+    // la class soon permet de décaler la slide concernée vers la gauche jusqu'à ce qu'elle sorte entièrement
+    // du carousel et soit donc caché par le overflow:hidden de ce dernier
     activeSlide.classList.add('soon');
 
+    // un premier timer qui va supprimer les class css residuelles
     setAnimationTimeout1(activeSlide);
 
     if(timeAnimationMobile2 != 'undefined'){
         clearTimeout(timeAnimationMobile2);
     }
 
+    // un second timer qui va afficher et faire venir de la droite la slide suivante
     timeAnimationMobile2 = setTimeout(function() {
         activeNextSlide(carouselSlidesMobile[orderNextMobile]);
     }, 500);
-
 
 
     if(timeAnimationMobile3 != 'undefined'){
         clearTimeout(timeAnimationMobile3);
     }
 
+    // un dernier timer qui va supprimer les dernieres classes résiduelles
     timeAnimationMobile3 = setTimeout(function() {
-        // activeAnimationMobileTimers++;
         removeNextClass(carouselSlidesMobile[orderNextMobile]);
     }, 1000);
 
-    // carouselSlidesMobile[orderNextMobile].classList.add('active');
-
+    // on met à jour l'indice de la slide active
     activeSlideMobile++;
-    activeSlideMobile=biggerThanFiveMobile(activeSlideMobile);
+    activeSlideMobile=biggerThanLengthMobile(activeSlideMobile);
 
 }
 
@@ -91,21 +106,27 @@ if (typeof(leftArrowMobile) != 'undefined' && leftArrowMobile != null)
     })
 }
 
+// fonction pour gèrer le roulement du carousel sur lui-même en direction de la droite
 function rollCarouselRightMobile(activeSlide){
 
+    // indice de la prochaine slide à afficher
     let orderPrevMobile = activeSlideMobile-1;
     orderPrevMobile=lessThanZeroMobile(orderPrevMobile);
 
+    // la class css prev permet de préparer la slide concerné à être affiché
     carouselSlidesMobile[orderPrevMobile].classList.add('prev');
 
+    // la class css prev-soon décale la slide concerné vers la droite
     activeSlide.classList.add('prev-soon');
 
+    // un premier timer qui va supprimer les class css residuelles
     setAnimationTimeout1Alt(activeSlide);
 
     if(timeAnimationMobile2 != 'undefined'){
         clearTimeout(timeAnimationMobile2);
     }
 
+    // un second timer qui va afficher et faire venir de la droite la slide suivante
     timeAnimationMobile2 = setTimeout(function() {
         activePrevSlide(carouselSlidesMobile[orderPrevMobile]);
     }, 500);
@@ -114,10 +135,12 @@ function rollCarouselRightMobile(activeSlide){
         clearTimeout(timeAnimationMobile3);
     }
 
+    // un dernier timer qui va supprimer les dernieres classes résiduelles
     timeAnimationMobile3 = setTimeout(function() {
         removePrevClass(carouselSlidesMobile[orderPrevMobile]);
     }, 1000);
 
+    // on met à jour l'indice de la slide active
     activeSlideMobile--;
     activeSlideMobile=lessThanZeroMobile(activeSlideMobile);
 
@@ -133,33 +156,27 @@ function lessThanZeroMobile(variable){
     return variable;
 }
 
-//vérifie si la variable n'a pas dépassé une limite (plus grand que 5) et la corrige si c'est le cas
-function biggerThanFiveMobile(variable){
-    if(variable>5){
-        variable-=6;
+//vérifie si la variable n'a pas dépassé une limite (plus grand que length de carouselSlidesMobile)
+// et la corrige si c'est le cas
+function biggerThanLengthMobile(variable){
+    if(variable>(carouselSlidesMobile.length-1)){
+        variable-=carouselSlidesMobile.length;
     }
 
     return variable;
 }
 
-
+//fonction qui clear l'ancien timer et en set un nouveau
 function setNewTimerMobile(){
-    // if(typeof time !== 'undefined'){
-    // if (isAnimationMobile) {
-    //     return;
-    // }
-    // isAnimationMobile = true;
 
     clearInterval(timeMobile);
-    // }
-    timeMobile = setInterval(nextSlideMobile,7000);
-    // return time;
 
-    // setTimeout(function() {
-    //     isAnimationMobile = false;
-    // }, 1000);  
+    timeMobile = setInterval(nextSlideMobile,7000);
+  
 }
 
+//fonction déclenché par le timer ci-dessus -> permet de déclencher automatiquement l'event de roulement du 
+//carousel vers la gauche
 function nextSlideMobile(){
     if (typeof(carouselSlidesMobile[activeSlideMobile]) != 'undefined' && carouselSlidesMobile[activeSlideMobile] != null)
     {
@@ -176,32 +193,39 @@ function nextSlideMobile(){
     }
 }
 
+//retire les class css soon et active
 function removeActiveAndSoonClasses(activeSlide){
     activeSlide.classList.remove('soon');
     activeSlide.classList.remove('active');
 }
 
+//retire les class css active et prev-soon
 function removeActiveAndPrevSoonClasses(activeSlide){
     activeSlide.classList.remove('prev-soon');
     activeSlide.classList.remove('active');
 }
 
+// active la slide suivante
 function activeNextSlide(nextSlide){
     nextSlide.classList.add('active');
 }
 
+//active la slide précédente
 function activePrevSlide(prevSlide){
     prevSlide.classList.add('active');
 }
 
+// retire la class css next
 function removeNextClass(nextSlide){
     nextSlide.classList.remove('next');
 }
 
+// retire la class css prev
 function removePrevClass(prevSlide){
     prevSlide.classList.remove('prev');
 }
 
+// timer qui va permettre de retirer certaines des classes css résiduelles suite à une animation vers la gauche
 function setAnimationTimeout1(activeSlide){
     if(timeAnimationMobile != 'undefined'){
         clearTimeout(timeAnimationMobile);
@@ -212,6 +236,7 @@ function setAnimationTimeout1(activeSlide){
     }, 1000);
 }
 
+// timer qui va permettre de retirer certaines des classes css résiduelles sutie à une animation vers la droite
 function setAnimationTimeout1Alt(activeSlide){
     if(timeAnimationMobile != 'undefined'){
         clearTimeout(timeAnimationMobile);
