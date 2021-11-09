a972573ade13a31fa233b954baa331835070515f
June <rem-je42@hotmail.fr>
2021-11-09 16:48:55 +0100

    Revert "Merge branch 'dev-june'"
    This reverts commit c5c29c247d687fa5aafcbd616f399161984a5ac9, reversing
    changes made to d7b2495766cddf4babf1ad505950b69d281666a0.

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -107,14 +107,9 @@ class ProfilController extends AbstractController
                         if($data['pseudo']!==null){
                             // si le pseudo n'est pas le même que l'utilisateur possède déjà
                             if($data['pseudo']!==$currentUser->getPseudo()){
-                                // le pseudo est-il déjà utilisé par un autre utilisateur ?
-                                if(!$this->isPseudoAlreadyUsed($data['pseudo'])){
-                                    $currentUser->setPseudo($data['pseudo']);
-                                    $isAction++;
-                                    $this->em->persist($currentUser);
-                                }else{
-                                    return $this->redirectToRoute('profilGeneral',['error'=>'pseudoNotAvailable']);
-                                }                            
+                                $currentUser->setPseudo($data['pseudo']);
+                                $isAction++;
+                                $this->em->persist($currentUser);
                             }                         
                         }  
 
@@ -159,18 +154,6 @@ class ProfilController extends AbstractController
     {
         return $this->render('profil/profil-stats.html.twig', [
         ]);
-    }
-
-
-    /////FUNCTION WITHOUT ROUTE HERE\\\\\
-
-    
-    private function isPseudoAlreadyUsed($pseudo)
-    {
-        $checkPseudo = $this->ur->findBy(['pseudo'=>$pseudo]);
-        if($checkPseudo!==[]){           
-            return true;
-        }
     }
 
 
