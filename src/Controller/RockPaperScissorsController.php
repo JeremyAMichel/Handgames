b397ebc1b655f74236a38d83f75a3b3276cb38be
June <rem-je42@hotmail.fr>
2021-11-10 15:34:30 +0100

    the skin displayed in RSC game is the one selected by the user to be the current one

--- src/Controller/RockPaperScissorsController.php
+++ src/Controller/RockPaperScissorsController.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class RockPaperScissorsController extends AbstractController
 {
@@ -22,14 +23,21 @@ class RockPaperScissorsController extends AbstractController
     private $userRepository;
 
     /**
+     * @var Security
+     */
+    private $security;
+
+    /**
      * HandBlackWhiteController constructor.
      * @param EntityManagerInterface $em
      * @param UserRepository $userRepository
+     * @param Security $security
      */
-    public function __construct(EntityManagerInterface $em, UserRepository $userRepository)
+    public function __construct(EntityManagerInterface $em, UserRepository $userRepository, Security $security)
     {
         $this->em = $em;
         $this->userRepository = $userRepository;
+        $this->security = $security;
     }
 
     /**
@@ -44,8 +52,19 @@ class RockPaperScissorsController extends AbstractController
         }
         else{
             if($mode == 'bot'){
-                return $this->render('rock_paper_scissors/bot.html.twig', [
+                /**
+                 * @var User
+                 */
+                $currentUser = $this->security->getUser();
+                $currentUserSkins = $currentUser->getUserSkins();
+                foreach($currentUserSkins as $UserSkin){
+                    if(true === $UserSkin->getIsActiveSkin()){
+                        $currentUserActiveSkin = $UserSkin->getSkin();
+                    }
+                }
 
+                return $this->render('rock_paper_scissors/bot.html.twig', [
+                    'userSkin' => $currentUserActiveSkin,
                 ]);
             }
         }
